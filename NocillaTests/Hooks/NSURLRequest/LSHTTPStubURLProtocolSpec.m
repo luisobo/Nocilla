dcc52c47221dd87c7ebfb1839fc53ed5e544fe1d
Patrick Cheng <patrickyccheng@gmail.com>
2014-12-07 12:03:21 -0800

    attempt to fix errors when building on Travis.
    changed the URL of the tests, just in case it was stepping on each other.
    
    called clearStubs

--- NocillaTests/Hooks/NSURLRequest/LSHTTPStubURLProtocolSpec.m
+++ NocillaTests/Hooks/NSURLRequest/LSHTTPStubURLProtocolSpec.m
@@ -46,14 +46,17 @@ SPEC_BEGIN(LSHTTPStubURLProtocolSpec)
 
 describe(@"+canInitWithRequest", ^{
 	context(@"when NOT catching all request", ^{
-		beforeEach(^{
+		beforeEach(^{			
 			[[LSNocilla sharedInstance] stub:@selector(catchAllRequests) andReturn:@YES];
+		});
+		afterEach(^{
+			[[LSNocilla sharedInstance] clearStubs];
 		});
 		context(@"when there is a stub response", ^{
 			it(@"should return YES", ^{
-				NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://api.example.com/dogs.json"]];
+				NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://api.example.com/horse.json"]];
 
-				LSStubRequest *stubRequest = [[LSStubRequest alloc] initWithMethod:@"GET" url:@"http://api.example.com/dogs.json"];
+				LSStubRequest *stubRequest = [[LSStubRequest alloc] initWithMethod:@"GET" url:@"http://api.example.com/horse.json"];
 				LSStubResponse *stubResponse = [[LSStubResponse alloc] initWithStatusCode:403];
 				[stubResponse setHeader:@"Content-Type" value:@"application/xml"];
 				NSString *bodyString = @"<error>Error</error>" ;
@@ -68,7 +71,7 @@ describe(@"+canInitWithRequest", ^{
 		});
 		context(@"when there is NO stub response", ^{
 			it(@"should return NO", ^{
-				NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://api.example.com/dogs.json"]];
+				NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://api.example.com/sheep.json"]];
 				
 				BOOL result = [LSHTTPStubURLProtocol canInitWithRequest:request];
 				
