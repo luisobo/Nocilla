c54d02f8aa3cafe44596718c9b8b05c2be3f0af7
Robert Böhnke <robb@robb.is>
2015-03-26 12:36:08 +0100

    Add framework target

--- Nocilla/Nocilla.h
+++ Nocilla/Nocilla.h
@@ -1,6 +1,20 @@
-#import <Foundation/Foundation.h>
+//
+//  Nocilla.h
+//  Nocilla
+//
+//  Created by Robert Böhnke on 26/03/15.
+//  Copyright (c) 2015 Luis Solano Bonet. All rights reserved.
+//
 
-#import "LSNocilla.h"
-#import "DSL.h"
-#import "NSString+Nocilla.h"
-#import "NSData+Nocilla.h"
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nocilla.
+FOUNDATION_EXPORT double NocillaVersionNumber;
+
+//! Project version string for Nocilla.
+FOUNDATION_EXPORT const unsigned char NocillaVersionString[];
+
+#import <Nocilla/LSNocilla.h>
+#import <Nocilla/DSL.h>
+#import <Nocilla/NSString+Nocilla.h>
+#import <Nocilla/NSData+Nocilla.h>
