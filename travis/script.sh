d317b2eac1626a375d204cea145abd577d95328f
Robert Böhnke <robb@robb.is>
2015-04-08 12:10:57 +0200

    Use xcodebuild instead of xctool

--- travis/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-xctool -configuration Debug -workspace Nocilla.xcworkspace -scheme Nocilla build test -sdk iphoneos
-xctool -configuration Debug -workspace Nocilla.xcworkspace -scheme Nocilla build test -test-sdk iphoneos -sdk iphoneos
